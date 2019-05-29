8acf100924466e2ff8f92cdb8bc3f5f98a493afc
insasquatchcountry <insasquatchcountry@gmail.com>
2019-05-28 17:49:27 -0700

    Delete tts5000.sh

--- tts5000.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-VALUE=${1?Error: no value given}
-plutil -convert json ~/Library/Preferences/com.apple.speech.voice.prefs.plist -o - | python -c 'import json, sys;d=json.load(sys.stdin);[x.__setitem__(-1, '$VALUE') for x in d["VoiceRateDataArray"]];json$
-echo "Text-To-Speech speed has been adjusted to $VALUE. Run 'rstts' to restart TTS and apply changes."
